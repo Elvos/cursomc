82d1e0fd433e10325c3459b047fc4a9a088b2a73
Elvis Barbieri <elvis@delogic.com.br>
2018-06-24 03:41:22 -0300

    Persistencia +BD

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,19 +1,27 @@
 package com.example.demo.resources;
 
-import org.springframework.boot.SpringApplication;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		 return "REST ESTA FUNCINANDO";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria catum = new Categoria(1, "informática");
+		Categoria catdois = new Categoria(2, "escritório");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(catum);
+		lista.add(catdois);
+		return lista;
 	}
-	
-	
 
 }
